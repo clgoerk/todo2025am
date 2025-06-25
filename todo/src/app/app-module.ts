a02a12a81832997efbf0ab136e39f6ae991179d5
clgoerk <Christopherlee.Goerk@triosstudent.com>
2025-06-25 08:32:24 -0400

    Final Version

---
+++ todo/src/app/app-module.ts
@@ -0,0 +1,17 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { App } from './app';
+
+@NgModule({
+
+  imports: [
+    BrowserModule, App
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
